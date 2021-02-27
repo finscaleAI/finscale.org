7e3a3ca04932b747aced3ab3e9c8807af6d29597
Ankit@Muellners <53999665+Ankit-muellner@users.noreply.github.com>
2021-02-27 09:46:18 +0100

    added documentation folder

---
+++ Documentation/js/app.js
@@ -0,0 +1,58 @@
+/* Template Name: Landrick - Saas & Software Landing Page Template
+   Author: Shreethemes
+   E-mail: shreethemes@gmail.com
+   Created: August 2019
+   File Description: Main JS file of the template Documentation
+*/
+
+
+(function ($) {
+
+    'use strict';
+    //Menu
+    $('.navbar-toggle').on('click', function (event) {
+        $(this).toggleClass('open');
+        $('#navigation').slideToggle(400);
+    });
+    
+    $('.navigation-menu>li').slice(-2).addClass('last-elements');
+    
+    $('.menu-arrow,.submenu-arrow').on('click', function (e) {
+        if ($(window).width() < 992) {
+            e.preventDefault();
+            $(this).parent('li').toggleClass('open').find('.submenu:first').toggleClass('open');
+        }
+    });
+    
+    // Smooth scroll 
+    $('.navbar-nav a').on('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 0
+        }, 3000, 'easeInOutExpo');
+        event.preventDefault();
+    });
+	// Add scroll class
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 50) {
+            $(".sticky").addClass("nav-sticky");
+        } else {
+            $(".sticky").removeClass("nav-sticky");
+        }
+    });
+    //ScrollBtn
+    $('.scrollbtn').on('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 72
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+	//Scrollspy
+    $(".navbar-nav").scrollspy({
+        offset: 20
+    });
+})(jQuery)
